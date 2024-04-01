054b170f1c8268f5dd7f2fe1f4b4f3c0684de464
Matthew (Matt) Meyer <meyermatt22@gmail.com>
2024-04-01 10:14:23 -0500

    image alt footer

--- src/Footer.js
+++ src/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
           target="_blank" rel="noreferrer"
           href="https://www.linkedin.com/in/matthew-meyer-59138588/"
         >
-          <img className="mattlink" src="https://i.imgur.com/TNBNGjd.png"></img>
+          <img className="mattlink" src="https://i.imgur.com/TNBNGjd.png" alt=''></img>
         </a>
       </div>
       <div>
@@ -17,7 +17,7 @@ function Footer() {
           target="_blank" rel="noreferrer"
           href="https://github.com/meyermatt22"
         >
-          <img className="mattlink" src="https://i.imgur.com/3crBXlp.png"></img>
+          <img className="mattlink" src="https://i.imgur.com/3crBXlp.png" alt=''></img>
         </a>
       </div>
       <div>
@@ -25,7 +25,7 @@ function Footer() {
           target="_blank" rel="noreferrer"
           href="https://meyermatt22.github.io/"
         >
-          <img className="mattlink" src="https://i.imgur.com/fTF4Qbr.png"></img>
+          <img className="mattlink" src="https://i.imgur.com/fTF4Qbr.png" alt=''></img>
         </a>
       </div>
     </div>
