630a47daa2d745614abf881a3a21fd84632708bc
Matthew (Matt) Meyer <meyermatt22@gmail.com>
2024-04-13 12:34:41 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import yolk from './yolk.png';
 import './App.css';
 import Footer from './Footer';
 import Header from './Header';
